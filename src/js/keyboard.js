f4d675766b77ee2c47e96305e5eb848298b3855b
chaeeun <aug0901@naver.com>
2022-03-24 16:24:22 +0900

    font select UI 추가

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,14 +1,16 @@
 
 // Dark Theme UI 
 export class Keyboard {
-    #swichEl;   // 앞에 #(해시)를 붙히면 privite class, 클래스 외부에서 값을 변경해도 바뀌지 않음 (ES2019)
+    #swichEl;   // swich엘리먼트. 앞에 #(해시)를 붙히면 privite class, 클래스 외부에서 값을 변경해도 바뀌지 않음 (ES2019)
+    #fontSelectEl;  // font select 엘리먼트
     constructor() {     // 생성자
         this.#assignElement();
         this.#addEvent();
     }
         
     #assignElement() {  // 엘리먼트를 가져온다
-        this.#swichEl = document.getElementById("switch");  // html의 swich 엘리먼트를 가져옴
+        this.#swichEl = document.getElementById("switch");  // html의 아이디값 swich 엘리먼트를 가져옴
+        this.#fontSelectEl = document.getElementById("font");   // html의 아이디값 font 엘리먼트를 가져옴
     }
 
     #addEvent() {   // 이벤트를 붙힌다
@@ -24,6 +26,15 @@ export class Keyboard {
             );
             // 콘솔창에 일어난 이벤트 타겟의 checked 여부를 알려주세요
             console.log(event.target.checked);
+        });
+
+        // fontSelect ui event
+        // 이 fontSelect 엘리먼트가 change 되는 이벤트가 일어나면
+        this.#fontSelectEl.addEventListener("change", (event) => {
+            // html의 body의 스타일 중 fontFamily를 이벤트 타겟의 value로 바꿔주세요
+            document.body.style.fontFamily = event.target.value;
+            // 콘솔창에 일어난 이벤트 타겟의 value 를 알려주세요
+            console.log(event.target.value);    // select박스는 value를 갖고올수있다
         });
     }
 
